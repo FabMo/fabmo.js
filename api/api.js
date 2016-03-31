f91cdbbfe72012ca8bcc2a65a2a30d30e14f15c3
Ryan Sturmer <ryasnturmer@gmail.com>
2016-03-31 10:59:18 -0400

    Updates

---
+++ api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "FabMoDashboard"
+    ],
+    "modules": [
+        "fabmo.js"
+    ],
+    "allModules": [
+        {
+            "displayName": "fabmo.js",
+            "name": "fabmo.js"
+        }
+    ],
+    "elements": []
+} };
+});
